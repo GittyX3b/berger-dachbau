a1877d419a37979089004102ad4a4d35b1b125ef
Dandy Nagel <github@dandynagel.com>
2026-03-25 13:48:37 +0100

    preparing deployment

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 export default defineConfig({
-  //base: "/", // set github reponame for deploy
+  base: "/berger-dachbau/", // set github reponame for deploy
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
+        main: path.resolve(__dirname, "index.html"),
       },
     },
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
     cssCodeSplit: false,
     cssMinify: true,
@@ -24,15 +24,15 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@data': path.resolve(__dirname, './src/data'),
-      '@context': path.resolve(__dirname, './src/data/context'),
-      '@layouts': path.resolve(__dirname, './src/layouts'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@elements': path.resolve(__dirname, './src/components/elements'),
-      '@public': path.resolve(__dirname, './public'),
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@data": path.resolve(__dirname, "./src/data"),
+      "@context": path.resolve(__dirname, "./src/data/context"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@elements": path.resolve(__dirname, "./src/components/elements"),
+      "@public": path.resolve(__dirname, "./public"),
     },
   },
   plugins: [react(), tailwindcss()],
